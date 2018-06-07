5d7674f86a1de928167810b185ff6d5546dc9d6c
Gurpreet Singh <gurpreet.charlie@gmail.com>
2018-06-08 01:59:40 +1200

    Added Book Model and add it to databases using code first migrations

---
+++ BookList_CRUD/BookList_CRUD/Migrations/20180607134917_Rectifying.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookList_CRUD.Migrations
+{
+    public partial class Rectifying : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
